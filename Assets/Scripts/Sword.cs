5c41d80c60a3dcc8de6a47a07305a64136fba432
Karhalll <mar.fucik@gmail.com>
2023-04-11 16:23:48 +0200

    Sword Both Directions

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,10 +6,14 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake() {
-        playerControls = new PlayerControls();
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
     }
 
     private void OnEnable() {
@@ -21,7 +25,27 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
     private void Attack() {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerPos.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else 
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
